f39337aad62c3b17d59de5c4fcc1920c37798197
romain.trimouille <romain.trimouille@iut-rodez.fr>
2023-11-30 15:05:49 +0100

    Initialisation

---
+++ app/src/main/java/fr/gr3/strovo/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.gr3.strovo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
