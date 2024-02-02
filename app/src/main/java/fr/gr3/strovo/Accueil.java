d5f2e5a28ccd1b04c6628ebad57b4c87ede514c5
Aymeric Thevenet <aymeric.thevenet@iut-rodez.fr>
2024-02-02 08:55:18 +0100

    Ajout du lien entre l'API et la liste des parcours

--- app/src/main/java/fr/gr3/strovo/Accueil.java
+++ app/src/main/java/fr/gr3/strovo/Accueil.java
@@ -14,53 +14,74 @@ import android.app.Dialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe représentant l'activité principale de l'application, correspondant à l'écran d'accueil.
+ * Cette activité affiche une liste de parcours et permet aux utilisateurs de rechercher, filtrer,
+ * et interagir avec les parcours affichés.
+ */
 public class Accueil extends AppCompatActivity {
 
-    private SearchView searchView;
+    /** URL de l'API pour récupérer la liste des parcours de l'utilisateur */
+    private final String URL_LISTE_PARCOURS = "http://172.20.10.14:8080/parcours/utilisateur/%d";
+
+    /** Composant graphique de la recherche */
+    private SearchView rechercheNom;
+
+    /** Composant graphique du bouton de filtre */
     private Button filterButton;
-    private ListView listView;
-    private Button floatingActionButton;
+
+    /** Composant graphique de la liste des parcours */
+    private ListView listViewParcours;
+
+    /** Composant graphique du bouton qui lance un enregistrement de parcours */
+    private Button lancerParcoursButton;
+
+
+    /** Liste des parcours de l'utilisateur */
+    private List<Parcours> parcoursList;
+
+    /** Adaptateur pour la liste des parcours */
+    private ParcoursAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
-        searchView = findViewById(R.id.search_view);
+        // Affectation des composants graphiques
+        rechercheNom = findViewById(R.id.search_view);
         filterButton = findViewById(R.id.filter_button);
-        listView = findViewById(R.id.list_view);
-        floatingActionButton = findViewById(R.id.floating_action_button);
+        listViewParcours = findViewById(R.id.list_view);
+        lancerParcoursButton = findViewById(R.id.floating_action_button);
 
-        ArrayList<Parcours> listItems = new ArrayList<>();
-        listItems.add(new Parcours("parcours1","12/12/2023 18:00"));
-        listItems.add(new Parcours("parcours2","12/12/2023 18:00"));
-        listItems.add(new Parcours("parcours3","12/12/2023 18:00"));
-//        listItems.add("\n1er parcours \n12/12/2023 18:00\n");
-//        listItems.add("\n2e parcours \n13/12/2023 18:00\n");
-//        listItems.add("\n3e parcours \n14/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
-//        listItems.add("\n5e parcours \n15/12/2023 18:00\n");
+        parcoursList = new ArrayList<>();
 
+        // Creation d'un adaptateur personnalisé
+        adapter = new ParcoursAdapter(this, R.layout.vue_item_liste, parcoursList);
+        listViewParcours.setAdapter(adapter);
 
-        ParcoursAdapter adapter = new ParcoursAdapter(this, R.layout.vue_item_liste, listItems);
-        listView.setAdapter(adapter);
+        // Appelle la méthode pour récupérer les données de l'API
+        fetchParcoursFromApi();
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        // Configuration de l'écouteur de la barre de recherche
+        rechercheNom.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                // Gérez la soumission de la requête de recherche ici
+            public boolean onQueryTextSubmit(String nameParcours) {
+
                 return false;
             }
 
@@ -71,6 +92,7 @@ public class Accueil extends AppCompatActivity {
             }
         });
 
+        // Configuration de l'écouteur du bouton de filtre
         filterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,66 +119,114 @@ public class Accueil extends AppCompatActivity {
                     }
                 });
 
-                // Gérez le clic sur le bouton "Annuler"
+                // Gère le clic sur le bouton "Annuler"
                 annuler.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Gérez le clic sur le bouton "Annuler" ici
                         dialog.dismiss();
                     }
                 });
 
-                // Affichez la fenêtre contextuelle
+                // Affiche la fenêtre contextuelle
                 dialog.show();
             }
 
         });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Configuration de l'écouteur du clic sur un élément de la liste
+        listViewParcours.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Gérez le clic sur un élément de la liste ici
+                // Gère le clic sur un élément de la liste
             }
         });
 
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        // Configuration de l'écouteur du clic sur le bouton d'action flottant
+        lancerParcoursButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Créez une instance de Dialog
+                // Créé une instance de Dialog
                 final Dialog dialog = new Dialog(Accueil.this);
 
-                // Définissez le contenu de la fenêtre contextuelle
+                // Définis le contenu de la fenêtre contextuelle
                 dialog.setContentView(R.layout.popup_lancer_course);
 
-                // Récupérez les éléments de la fenêtre contextuelle
+                // Récupére les éléments de la fenêtre contextuelle
                 EditText inputCommentaire = dialog.findViewById(R.id.inputCommentaire);
                 Button confirmer = dialog.findViewById(R.id.confirmer);
                 Button annuler = dialog.findViewById(R.id.annuler);
 
-                // Gérez le clic sur le bouton "Confirmer"
+                // Gère le clic sur le bouton "Confirmer"
                 confirmer.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Gérez le clic sur le bouton "Confirmer" ici
                         Intent intent = new Intent(Accueil.this, MapActivity.class);
                         startActivity(intent);
                         dialog.dismiss();
                     }
                 });
 
-                // Gérez le clic sur le bouton "Annuler"
+                // Gère le clic sur le bouton "Annuler"
                 annuler.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Gérez le clic sur le bouton "Annuler" ici
                         dialog.dismiss();
                     }
                 });
 
-                // Affichez la fenêtre contextuelle
+                // Affiche la fenêtre contextuelle
                 dialog.show();
             }
         });
+    }
+
+    /**
+     * Récupère les données des parcours depuis l'API en utilisant la bibliothèque Volley.
+     */
+    private void fetchParcoursFromApi() {
+        // TODO Récupérer l'identifiant de l'utilisateur
+        int userId = 1;
+        String.format(URL_LISTE_PARCOURS, userId);
+        String apiUrl = "http://172.20.10.14:8080/parcours/utilisateur/1";
+
+        // Utilisation de la bibliothèque Volley pour effectuer la requête HTTP
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                apiUrl,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        parseJsonResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("ERREUR : " + error.getMessage());
+                    }
+                });
+
+        // Ajoute la requête à la file d'attente
+        queue.add(jsonArrayRequest);
+    }
+
+    /**
+     * Analyse la réponse JSON de l'API et peuple la liste des parcours.
+     * @param response Un objet de type JSONArray
+     */
+    private void parseJsonResponse(JSONArray response) {
+        for (int i = 0; i < response.length(); i++) {
+            try {
+                JSONObject parcoursJson = response.getJSONObject(i);
+                Parcours parcours = new Parcours(parcoursJson.getString("name"),
+                                                 parcoursJson.getString("date"));
+                parcoursList.add(parcours);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
