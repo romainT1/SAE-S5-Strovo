c37b64b4171ff3f568966b3271ee82c364f43500
Romain TRIMOUILLE <romain.trimouille@iut-rodez.fr>
2024-02-09 09:17:36 +0100

    Ajout du model token

---
+++ app/src/main/java/fr/gr3/strovo/model/Token.java
@@ -0,0 +1,17 @@
+package fr.gr3.strovo.model;
+
+public class Token {
+    private String value;
+
+    public Token(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
